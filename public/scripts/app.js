b3c627a157aa6f10b64c17642a25328962e2f661
Margarita Amy Wang <amywang.2@alumni.ubc.ca>
2017-12-21 20:23:56 +0000

    changed routes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,14 @@
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });
+    url: "/"
+  }).done();
   function initMap(){
     //Map options
     var options = {
       zoom:13,
       center: {lat:49.2827,lng:-123.1207}
-    } 
+    }
     //New map
     var map = new google.maps.Map(document.getElementById('map'), options);
 
@@ -21,7 +17,7 @@ $(() => {
       addMarker({coords:event.latLng});
     });
     /*
-    
+
     var infoWindow = new google.maps.InfoWindow({
       content:'<h1>New Westminster</h1>'
     });
@@ -32,7 +28,7 @@ $(() => {
     */
 
     addMarker({
-      coords:{lat:49.2819, lng:-123.1083}, 
+      coords:{lat:49.2819, lng:-123.1083},
       content: '<h2>Lighthouse Labs</h2>'
       });
 
@@ -40,7 +36,7 @@ $(() => {
       var marker = new google.maps.Marker({
       position:props.coords,
       map:map
-       
+
       });
 
       if(props.content){
@@ -53,5 +49,5 @@ $(() => {
         });
       }
     }
-  }  
+  }
 });
