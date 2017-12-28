0f7870f8921bb5bc794556871c9281f756fab08c
ItsJustChewbacca <itsjustchewbacca@gmail.com>
2017-12-28 22:04:18 +0000

    no changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 const cookieSession = require('cookie-session');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
